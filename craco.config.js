8b36cd57642d098a5bd8e68cb18df371a09460a3
miyabayt <4364793+miyabayt@users.noreply.github.com>
2023-11-08 19:49:06 +0900

    fix: Jestのテストが通るように修正する

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,30 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   babel: {
     presets: [
+      '@babel/preset-env',
       [
-        "@babel/preset-react",
-        { runtime: "automatic", importSource: "@emotion/react" },
+        '@babel/preset-react',
+        { runtime: 'automatic', importSource: '@emotion/react' },
       ],
+      '@babel/preset-typescript',
     ],
-    plugins: ["@emotion/babel-plugin"],
+    plugins: ['@emotion/babel-plugin'],
   },
   webpack: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
     },
   },
-};
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      moduleNameMapper: {
+        '^axios$': 'axios/dist/node/axios.cjs',
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+  },
+}
