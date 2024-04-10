e3b338b079d801e60e3924157b62bfc0af2755bf
miyabayt <4364793+miyabayt@users.noreply.github.com>
2024-04-10 18:31:18 +0900

    fix: トークンリフレッシュの重複実行を防ぐ

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,8 +4,6 @@ import Cookie from 'js-cookie'
 import refresh from '@/services/auth/refresh'
 import { isTokenExpired } from '@/utils/jwt'
 
-let isRefreshing = false
-
 const createAxiosInstance = () => {
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -20,14 +18,8 @@ const createAxiosInstance = () => {
     const accessToken = Cookie.get('access_token')
     const refreshToken = Cookie.get('refresh_token')
     let newAccessToken = accessToken
-    if (
-      !isRefreshing &&
-      accessToken &&
-      refreshToken &&
-      isTokenExpired(accessToken)
-    ) {
+    if (accessToken && refreshToken && isTokenExpired(accessToken)) {
       try {
-        isRefreshing = true
         console.log('try to refresh token...')
         const { data, success } = await refresh(accessToken, refreshToken)
         if (success) {
@@ -36,8 +28,6 @@ const createAxiosInstance = () => {
         }
       } catch (e) {
         console.log('failed to refresh token.')
-      } finally {
-        isRefreshing = false
       }
     }
 
